f1a158e4157502c8e8c77656102550d044562e50
eozhmegov <banezmesm@gmail.com>
2023-07-22 08:38:33 +0000

    errors fixed

--- backup_restore.sh
+++ backup_restore.sh
@@ -1,18 +1,16 @@
 #!/bin/bash -e
 
-DATABASES=`ls | grep *_db`
+DATABASE='wordpress_db'
 USER='wordpress'
 PASS='wordpress'
 DIR="/tmp/backup"
 
-for db in $DATABASES;
-do
-for filename in $DIR/$db/*.txt;
+for filename in $DIR/$DATABASE/*.txt;
 do
 tablename=`basename $filename .txt`
-mysql --user=$USER --password=$PASS $db << EOF
+export MYSQL_PWD=$PASS; mysql -u $USER -h 10.110.1.130 --local-infile=1 $DATABASE < $DIR/$DATABASE/$tablename.sql;
+export MYSQL_PWD=$PASS; mysql -u $USER -h 10.110.1.130 --local-infile=1 $DATABASE << EOF
   SET FOREIGN_KEY_CHECKS=0;
-  LOAD DATA INFILE '$filename' INTO TABLE \`$tablename\`;
+  LOAD DATA LOCAL INFILE '$filename' INTO TABLE \`$tablename\`;
 EOF
-done
 done
